5bee9ace12356cc0eb1ecf319032191eb91d54ad
oscaroceguera <oscaroceguera@gmail.com>
2018-09-24 02:54:37 -0500

    Added create issue UI

--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Loadable from 'react-loadable'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 const App = Loadable({
   loader: () => import('./containers/App'),
-  loading: () => <div style={{ textAlign: 'center' }}>CARGANDO...</div>
+  loading: () => <div style={{ textAlign: 'center' }}><CircularProgress /></div>
 })
 
 ReactDOM.render(
